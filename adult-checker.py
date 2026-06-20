08718afc652dd5494853d2e25d6d832657662ae7
MASKY-33 <masky.somebody33@gmail.com>
2026-06-20 16:27:40 +0200

    Create adult-checker.py

---
+++ adult-checker.py
@@ -0,0 +1,11 @@
+## This Program:
+# - asks user's age
+# - checks (if) user is adult, (else) not adult
+
+
+user_age = int(input("Give in your age: "))
+
+if user_age >= 25:
+    print("You're an adult!")
+else:
+    print("You are not an adult yet.")
